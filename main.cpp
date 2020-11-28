037990b4d4fbdfa1a6fdc93dd87f2ce1186e46ca
planetmbit <jamming6123@S-MacBookPro.local>
2020-11-28 17:52:12 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE_MS 200
+
+
+int main()
+{
+	// Initialise the digital pin LED1 as an output
+	DigitalOut led(LED1);
+	
+	while (true) {
+		led = !led;
+		thread_sleep_for(BLINKING_RATE_MS);
+	}
+}
